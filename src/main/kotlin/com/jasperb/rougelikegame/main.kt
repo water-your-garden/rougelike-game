e20f0f4611e86b29a1c5e86a120623479f3dad2c
Jasper Ballantyne <jasperballantyne@MBP.local>
2020-04-11 11:17:04 +1200

    project setup

---
+++ src/main/kotlin/com/jasperb/rougelikegame/main.kt
@@ -0,0 +1,22 @@
+package com.jasperb.rougelikegame
+
+import org.hexworks.zircon.api.ColorThemes
+import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.Screens
+import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.component.ComponentAlignment
+
+@Suppress("ConstantConditionIf")
+fun main(args: Array<String>) {
+
+    val grid = SwingApplications.startTileGrid()
+    val screen = Screens.createScreenFor(grid)
+
+    screen.addComponent(Components.header()
+            .withText("Welcome")
+            .withAlignmentWithin(screen, ComponentAlignment.CENTER))
+
+    screen.applyColorTheme(ColorThemes.arc())
+    screen.display()
+
+}
