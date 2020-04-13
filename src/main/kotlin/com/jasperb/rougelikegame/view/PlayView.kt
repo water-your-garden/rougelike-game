ce963b9e7942f79997899e5c18edb1f43c058882
Jasper Ballantyne <jasperballantyne@MBP.local>
2020-04-14 08:12:12 +1200

    setup sidebar and log area on play view

--- src/main/kotlin/com/jasperb/rougelikegame/view/PlayView.kt
+++ src/main/kotlin/com/jasperb/rougelikegame/view/PlayView.kt
@@ -1,7 +1,8 @@
 package com.jasperb.rougelikegame.view
 
 import com.jasperb.rougelikegame.GameConfig
-import org.hexworks.zircon.api.ColorThemes
+import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.component.ComponentAlignment
 import org.hexworks.zircon.api.mvc.base.BaseView
 
 class PlayView : BaseView() {
@@ -11,7 +12,20 @@ class PlayView : BaseView() {
     override fun onDock() {
         super.onDock()
 
+        val sidebar = Components.panel()
+                .withSize(GameConfig.SIDEBAR_WIDTH, GameConfig.WINDOW_HEIGHT)
+                .wrapWithBox()
+                .build()
 
+        val logArea = Components.logArea()
+                .withTitle("Log")
+                .wrapWithBox()
+                .withSize(GameConfig.WINDOW_WIDTH - GameConfig.SIDEBAR_WIDTH, GameConfig.LOG_AREA_HEIGHT)
+                .withAlignmentWithin(screen, ComponentAlignment.BOTTOM_RIGHT)
+                .build()
+
+        screen.addComponent(sidebar)
+        screen.addComponent(logArea)
 
     }
 }
