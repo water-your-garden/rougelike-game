d10bbe1bbeafe2765bc016d4c3993e5e7a1dc867
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-29 08:03:34 +1200

    add gameContext

---
+++ src/main/kotlin/com/jasperb/rougelikegame/world/GameContext.kt
@@ -0,0 +1,12 @@
+package com.jasperb.rougelikegame.world
+
+import com.jasperb.rougelikegame.attributes.types.Player
+import com.jasperb.rougelikegame.extensions.GameEntity
+import org.hexworks.amethyst.api.Context
+import org.hexworks.zircon.api.screen.Screen
+import org.w3c.dom.events.UIEvent
+
+data class GameContext(val world: World,
+                       val screen: Screen,
+                       val uiEvent: UIEvent,
+                       val player: GameEntity<Player>) : Context
