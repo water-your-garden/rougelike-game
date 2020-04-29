a65288a4ac50d6c9c8236bc1ec3c0d975d5bc77c
Jasper Ballantyne <jasper.ballantyne@gmail.com>
2020-04-30 07:58:41 +1200

    enable GameBlocks to hold entities and make world able to add entities to empty blocks within the world

--- src/main/kotlin/com/jasperb/rougelikegame/blocks/GameBlock.kt
+++ src/main/kotlin/com/jasperb/rougelikegame/blocks/GameBlock.kt
@@ -1,11 +1,16 @@
 package com.jasperb.rougelikegame.blocks
 
 import com.jasperb.rougelikegame.builders.GameTileRepository
+import com.jasperb.rougelikegame.extensions.GameEntity
+import com.jasperb.rougelikegame.extensions.tile
+import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.zircon.api.data.BlockSide
 import org.hexworks.zircon.api.data.Tile
 import org.hexworks.zircon.api.data.base.BlockBase
 
-class GameBlock(private var defaultTile: Tile = GameTileRepository.FLOOR) : BlockBase<Tile>() {
+class GameBlock(private var defaultTile: Tile = GameTileRepository.FLOOR,
+                private val currentEntities: MutableList<GameEntity<EntityType>> = mutableListOf())
+    : BlockBase<Tile>() {
 
     val isFloor: Boolean
         get() = defaultTile == GameTileRepository.FLOOR
@@ -13,8 +18,30 @@ class GameBlock(private var defaultTile: Tile = GameTileRepository.FLOOR) : Bloc
     val isWall: Boolean
         get() = defaultTile == GameTileRepository.WALL
 
+    val isEmptyFloor: Boolean
+        get() = currentEntities.isEmpty()
+
+    val entities: Iterable<GameEntity<EntityType>>
+        get() = currentEntities.toList()
+
     override val layers: MutableList<Tile>
-        get() = mutableListOf(defaultTile)
+        get() {
+            val entityTiles = currentEntities.map { it.tile }
+            val tile = when {
+                entityTiles.contains(GameTileRepository.PLAYER) -> GameTileRepository.PLAYER
+                entityTiles.isNotEmpty() -> entityTiles.first()
+                else -> defaultTile
+            }
+            return mutableListOf(tile)
+        }
+
+    fun addEntity(entity: GameEntity<EntityType>) {
+        currentEntities.add(entity)
+    }
+
+    fun removeEntity(entity: GameEntity<EntityType>) {
+        currentEntities.remove(entity)
+    }
 
     override fun fetchSide(side: BlockSide): Tile {
         return GameTileRepository.EMPTY
--- src/main/kotlin/com/jasperb/rougelikegame/world/World.kt
+++ src/main/kotlin/com/jasperb/rougelikegame/world/World.kt
@@ -2,6 +2,16 @@ package com.jasperb.rougelikegame.world
 
 import com.jasperb.rougelikegame.blocks.GameBlock
 import com.jasperb.rougelikegame.builders.GameBlockFactory
+import com.jasperb.rougelikegame.extensions.GameEntity
+import com.jasperb.rougelikegame.extensions.position
+import org.hexworks.amethyst.api.Engine
+import org.hexworks.amethyst.api.Engines
+import org.hexworks.amethyst.api.entity.Entity
+import org.hexworks.amethyst.api.entity.EntityType
+import org.hexworks.cobalt.datatypes.Maybe
+import org.hexworks.cobalt.datatypes.extensions.fold
+import org.hexworks.cobalt.datatypes.extensions.map
+import org.hexworks.zircon.api.Positions
 import org.hexworks.zircon.api.builder.game.GameAreaBuilder
 import org.hexworks.zircon.api.data.Tile
 import org.hexworks.zircon.api.data.impl.Position3D
@@ -16,11 +26,58 @@ class World(startingBlocks: Map<Position3D, GameBlock>, visibleSize: Size3D, act
         .withLayersPerBlock(1)
         .build() {
 
+    private val engine: Engine<GameContext> = Engines.newEngine()
+
     init {
         // Sets the Tiles at the given position.
         startingBlocks.forEach { (pos, block) ->
             setBlockAt(pos, block)
+            block.entities.forEach { entity ->
+                engine.addEntity(entity)
+                entity.position = pos
+            }
         }
+    }
+
+    fun addEntity(entity: Entity<EntityType, GameContext>, position: Position3D) {
+        entity.position = position
+        engine.addEntity(entity)
+        fetchBlockAt(position).map {
+            it.addEntity(entity)
+        }
+    }
+
+    fun addAtEmptyPosition(entity: GameEntity<EntityType>,
+                           offset: Position3D = Positions.default3DPosition(),
+                           size: Size3D = actualSize()): Boolean {
+        return findEmptyLocationWithin(offset, size).fold(
+                whenEmpty = {
+                    false
+                },
+                whenPresent = { location ->
+                    addEntity(entity, location)
+                    true
+                }
+        )
+    }
+
+    fun findEmptyLocationWithin(offset: Position3D, size: Size3D): Maybe<Position3D> {
+        var position = Maybe.empty<Position3D>()
+        val maxTries = 10
+        var currentTry = 0
+        while (position.isPresent.not() && currentTry < maxTries) {
+            val pos = Positions.create3DPosition(
+                    x = (Math.random() * size.xLength).toInt() + offset.x,
+                    y = (Math.random() * size.yLength).toInt() + offset.y,
+                    z = (Math.random() * size.zLength).toInt() + offset.z)
+            fetchBlockAt(pos).map {
+                if (it.isEmptyFloor) {
+                    position = Maybe.of(pos)
+                }
+            }
+            currentTry++
+        }
+        return position
     }
 
     companion object {
